ddca16cae3c1a6d596610d577fdc7ad9e59553fb
rianfrancis <rianfrancis.cabanero@sun-asterisk.com>
2024-06-21 18:30:28 +0800

    To be fixed (finished the caching part but couldn't get redis to work)

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -3,18 +3,30 @@ import {
   Controller,
   Delete,
   Get,
+  Inject,
   Param,
   Post,
   Put,
   UseGuards,
+  UseInterceptors,
 } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { ProductCreateDto } from './dtos/product-create.dto';
 import { AuthGuard } from 'src/auth/auth.guard';
+import {
+  CACHE_MANAGER,
+  CacheInterceptor,
+  CacheKey,
+  CacheTTL,
+} from '@nestjs/cache-manager';
+import { Cache } from 'cache-manager';
 
 @Controller()
 export class ProductController {
-  constructor(private readonly productService: ProductService) {}
+  constructor(
+    private readonly productService: ProductService,
+    @Inject(CACHE_MANAGER) private cacheManager: Cache,
+  ) {}
   @UseGuards(AuthGuard)
   @Get('admin/products')
   async all() {
@@ -41,5 +53,24 @@ export class ProductController {
   @Delete('admin/products/:id')
   async delete(@Param('id') id: number) {
     return this.productService.delete(id);
+  }
+
+  @CacheKey('products_frontend')
+  @CacheTTL(30 * 60)
+  @UseInterceptors(CacheInterceptor)
+  @Get('ambassador/products/frontend')
+  async frontend() {
+    return this.productService.find();
+  }
+
+  @Get('ambassador/products/backend')
+  async backend() {
+    let products = await this.cacheManager.get('products_backend');
+    if (!products) {
+      products = await this.productService.find();
+
+      await this.cacheManager.set('products_backend', products, 1800);
+    }
+    return products;
   }
 }
--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -1,10 +1,17 @@
+import { CacheModule } from '@nestjs/cache-manager';
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
+import * as redisStore from 'cache-manager-redis-store';
 
 @Module({
   imports: [
     JwtModule.register({ secret: 'secret', signOptions: { expiresIn: '1d' } }),
+    CacheModule.register({
+      store: redisStore,
+      hostL: 'redis',
+      port: 6379,
+    }),
   ],
-  exports: [JwtModule],
+  exports: [JwtModule, CacheModule],
 })
 export class SharedModule {}
