f34045b2586039a96f1778da2e71db937e40c978
rianfrancis <rianfrancis.cabanero@sun-asterisk.com>
2024-06-24 11:33:56 +0800

    finished stats section

--- src/link/link.controller.ts
+++ src/link/link.controller.ts
@@ -11,6 +11,8 @@ import { LinkService } from './link.service';
 import { AuthGuard } from 'src/auth/auth.guard';
 import { AuthService } from 'src/auth/auth.service';
 import { Request } from 'express';
+import { Link } from './link';
+import { Order } from 'src/order/order';
 
 @Controller()
 export class LinkController {
@@ -36,6 +38,26 @@ export class LinkController {
       code: Math.random().toString(36).substring(6),
       user,
       products: products.map((id) => ({ id })),
+    });
+  }
+
+  @UseGuards(AuthGuard)
+  @Get('ambassador/stats')
+  async stats(@Req() request: Request) {
+    const user = await this.authService.user(request);
+
+    const links: Link[] = await this.linkservice.find({
+      user,
+      relations: ['orders'],
+    });
+
+    return links.map((link) => {
+      const completedOrders: Order[] = link.orders.filter((o) => o.complete);
+      return {
+        code: link.code,
+        count: completedOrders.length,
+        revenue: completedOrders.reduce((s, o) => s + o.ambassador_revenue, 0),
+      };
     });
   }
 }
